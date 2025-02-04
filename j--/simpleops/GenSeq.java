4150c8890edc4a23bd59200afc49a2a880d158ab
Jason40004 <148589763+JasonCourtois@users.noreply.github.com>
2025-02-04 15:58:43 -0500

    Init commit

---
+++ j--/simpleops/GenSeq.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+import jminusminus.CLEmitter;
+
+import static jminusminus.CLConstants.*;
+
+public class GenSeq {
+    public static void main(String[] args) {
+	...
+    }
+}
---
+++ j--/simpleops/Seq.java
@@ -0,0 +1,8 @@
+import java.lang.Integer;
+import java.lang.System;
+
+public class Seq {
+    public static void main(String[] args) {
+	...
+    }
+}
