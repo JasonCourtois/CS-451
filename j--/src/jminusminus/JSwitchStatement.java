0be80655a41621f5f6d6b0437d53c99cca6aeec7
Jason40004 <148589763+JasonCourtois@users.noreply.github.com>
2025-04-25 16:50:31 -0400

    halfway done with switch statements

--- j--/src/jminusminus/JSwitchStatement.java
+++ j--/src/jminusminus/JSwitchStatement.java
@@ -1,5 +1,8 @@
 package jminusminus;
 
+import static jminusminus.CLConstants.LOOKUPSWITCH;
+import static jminusminus.CLConstants.TABLESWITCH;
+
 import java.util.ArrayList;
 
 /**
@@ -18,10 +21,19 @@ class JSwitchStatement extends JStatement {
     // Stores the name of the break label
     private String breakLabel;
 
+    private int hi = Integer.MIN_VALUE;
+
+    private int lo = Integer.MAX_VALUE;
+
+    private int nLabels = 0;
+
+    private int opcode;
+
     /**
      * Constructs an AST node for a switch-statement.
      *
-     * @param line             line in which the switch-statement occurs in the source file.
+     * @param line             line in which the switch-statement occurs in the
+     *                         source file.
      * @param condition        test expression.
      * @param switchStmtGroups list of statement groups.
      */
@@ -32,7 +44,8 @@ class JSwitchStatement extends JStatement {
     }
 
     /**
-     * Sets the hasBreak variable to true, signifying that the control flow statement has a break in it.
+     * Sets the hasBreak variable to true, signifying that the control flow
+     * statement has a break in it.
      */
     public void hasBreak() {
         hasBreak = true;
@@ -49,8 +62,62 @@ class JSwitchStatement extends JStatement {
      * {@inheritDoc}
      */
     public JStatement analyze(Context context) {
-        // Push this instance into JMember enclosing statement
+        // Push this instance into JMember enclosing statement.
         JMember.enclosingStatement.push(this);
+
+        // Create a new local context for this switch statement.
+        LocalContext localContext = new LocalContext(context);
+
+
+        // Analyze the condition and verify that it is an integer.
+        condition = condition.analyze(localContext);
+        condition.type().mustMatchExpected(line, Type.INT);
+
+        // First check if there are switch statement groups present.
+        if (switchStmtGroups != null) {
+            // Loop through all groups.
+            for (SwitchStatementGroup group : switchStmtGroups) {
+                // Loop through all labels, analyze them and confirm they are integers.
+                for (JExpression label : group.getSwitchLabels()) {
+                    // Increment the total number of labels
+                    nLabels ++;
+
+                    // The only situation where label is null is for the default case:
+                    if (label == null) {
+                        continue;
+                    }
+
+                    // Analyze label and verify it is an int.
+                    label = label.analyze(localContext);
+                    label.type().mustMatchExpected(line, Type.INT);
+
+                    // Get the value of the integer, then compare it to the hi and lo values.
+                    int labelValue = ((JLiteralInt) label).toInt();
+                    if (labelValue > hi) {
+                        hi = labelValue;
+                    }
+                    if (labelValue < lo) {
+                        lo = labelValue;
+                    }
+                }
+
+                // If there are block statements present, analyze them as well.
+                if (group.block() != null) {
+                    for (JStatement block : group.block()) {
+                        block.analyze(localContext);
+                    }
+                }
+            }
+
+            // Compute the correct opcode that must be used for this switch statement
+            long tableSpaceCost = 5 + hi - lo ;
+            long tableTimeCost = 3;
+            long lookupSpaceCost = 3 + 2 * nLabels ;
+            long lookupTimeCost = nLabels ;
+            opcode = nLabels > 0 &&
+            ( tableSpaceCost + 3 * tableTimeCost <= lookupSpaceCost + 3 * lookupTimeCost ) ?
+            TABLESWITCH : LOOKUPSWITCH ;
+        }
 
         // Pop this instance into JMember enclosing statement
         JMember.enclosingStatement.pop();
@@ -66,7 +133,12 @@ class JSwitchStatement extends JStatement {
             breakLabel = output.createLabel();
         }
 
-        // TODO: break is totally different lol
+        if (opcode == TABLESWITCH) {
+            
+        } else if (opcode == LOOKUPSWITCH) {
+
+        }
+        
         if (hasBreak) {
             output.addLabel(breakLabel);
         }
@@ -88,7 +160,8 @@ class JSwitchStatement extends JStatement {
 }
 
 /**
- * A switch-statement group consists of a list of switch labels and a block of statements.
+ * A switch-statement group consists of a list of switch labels and a block of
+ * statements.
  */
 class SwitchStatementGroup {
     // Switch labels.
